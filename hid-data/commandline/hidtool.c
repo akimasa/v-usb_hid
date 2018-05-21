202504215a3d18fafc2b58195204e09721cd061a
Akimasa Tateba <akimasa@gmail.com>
2018-05-21 10:15:04 +0900

    initial commit

---
+++ hid-data/commandline/hidtool.c
@@ -0,0 +1,175 @@
+/* Name: hidtool.c
+ * Project: hid-data example
+ * Author: Christian Starkjohann
+ * Creation Date: 2008-04-11
+ * Tabsize: 4
+ * Copyright: (c) 2008 by OBJECTIVE DEVELOPMENT Software GmbH
+ * License: GNU GPL v2 (see License.txt), GNU GPL v3 or proprietary (CommercialLicense.txt)
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "hiddata.h"
+/* -------------------------- Device Description --------------------------- */
+
+#define  USB_CFG_VENDOR_ID       0xc0, 0x16 /* = 0x16c0 = 5824 = voti.nl */
+/* USB vendor ID for the device, low byte first. If you have registered your
+ * own Vendor ID, define it here. Otherwise you may use one of obdev's free
+ * shared VID/PID pairs. Be sure to read USB-IDs-for-free.txt for rules!
+ * *** IMPORTANT NOTE ***
+ * This template uses obdev's shared VID/PID pair for Vendor Class devices
+ * with libusb: 0x16c0/0x5dc.  Use this VID/PID pair ONLY if you understand
+ * the implications!
+ */
+#define  USB_CFG_DEVICE_ID       0xdf, 0x05 /* obdev's shared PID for HIDs */
+/* This is the ID of the product, low byte first. It is interpreted in the
+ * scope of the vendor ID. If you have registered your own VID with usb.org
+ * or if you have licensed a PID from somebody else, define it here. Otherwise
+ * you may use one of obdev's free shared VID/PID pairs. See the file
+ * USB-IDs-for-free.txt for details!
+ * *** IMPORTANT NOTE ***
+ * This template uses obdev's shared VID/PID pair for Vendor Class devices
+ * with libusb: 0x16c0/0x5dc.  Use this VID/PID pair ONLY if you understand
+ * the implications!
+ */
+#define USB_CFG_DEVICE_VERSION  0x00, 0x01
+/* Version number of the device: Minor number first, then major number.
+ */
+#define USB_CFG_VENDOR_NAME     'o', 'b', 'd', 'e', 'v', '.', 'a', 't'
+#define USB_CFG_VENDOR_NAME_LEN 8
+/* These two values define the vendor name returned by the USB device. The name
+ * must be given as a list of characters under single quotes. The characters
+ * are interpreted as Unicode (UTF-16) entities.
+ * If you don't want a vendor name string, undefine these macros.
+ * ALWAYS define a vendor name containing your Internet domain name if you use
+ * obdev's free shared VID/PID pair. See the file USB-IDs-for-free.txt for
+ * details.
+ */
+#define USB_CFG_DEVICE_NAME     'D', 'a', 't', 'a', 'S', 't', 'o', 'r', 'e'
+#define USB_CFG_DEVICE_NAME_LEN 9
+/* Same as above for the device name. If you don't want a device name, undefine
+ * the macros. See the file USB-IDs-for-free.txt before you assign a name if
+ * you use a shared VID/PID.
+ */
+/* ------------------------------------------------------------------------- */
+
+static char *usbErrorMessage(int errCode)
+{
+static char buffer[80];
+
+    switch(errCode){
+        case USBOPEN_ERR_ACCESS:      return "Access to device denied";
+        case USBOPEN_ERR_NOTFOUND:    return "The specified device was not found";
+        case USBOPEN_ERR_IO:          return "Communication error with device";
+        default:
+            sprintf(buffer, "Unknown USB error %d", errCode);
+            return buffer;
+    }
+    return NULL;    /* not reached */
+}
+
+static usbDevice_t  *openDevice(void)
+{
+usbDevice_t     *dev = NULL;
+unsigned char   rawVid[2] = {USB_CFG_VENDOR_ID}, rawPid[2] = {USB_CFG_DEVICE_ID};
+char            vendorName[] = {USB_CFG_VENDOR_NAME, 0}, productName[] = {USB_CFG_DEVICE_NAME, 0};
+int             vid = rawVid[0] + 256 * rawVid[1];
+int             pid = rawPid[0] + 256 * rawPid[1];
+int             err;
+
+    if((err = usbhidOpenDevice(&dev, vid, vendorName, pid, productName, 0)) != 0){
+        fprintf(stderr, "error finding %s: %s\n", productName, usbErrorMessage(err));
+        return NULL;
+    }
+    return dev;
+}
+
+/* ------------------------------------------------------------------------- */
+
+static void hexdump(char *buffer, int len)
+{
+int     i;
+FILE    *fp = stdout;
+
+    for(i = 0; i < len; i++){
+        if(i != 0){
+            if(i % 16 == 0){
+                fprintf(fp, "\n");
+            }else{
+                fprintf(fp, " ");
+            }
+        }
+        fprintf(fp, "0x%02x", buffer[i] & 0xff);
+    }
+    if(i != 0)
+        fprintf(fp, "\n");
+}
+
+static int  hexread(char *buffer, char *string, int buflen)
+{
+char    *s;
+int     pos = 0;
+
+    while((s = strtok(string, ", ")) != NULL && pos < buflen){
+        string = NULL;
+        buffer[pos++] = (char)strtol(s, NULL, 0);
+    }
+    return pos;
+}
+
+/* ------------------------------------------------------------------------- */
+
+static void usage(char *myName)
+{
+    fprintf(stderr, "usage:\n");
+    fprintf(stderr, "  %s read\n", myName);
+    fprintf(stderr, "  %s write <listofbytes>\n", myName);
+}
+
+int main(int argc, char **argv)
+{
+usbDevice_t *dev;
+char        buffer[129];    /* room for dummy report ID */
+int         err;
+
+    if(argc < 2){
+        usage(argv[0]);
+        exit(1);
+    }
+    if((dev = openDevice()) == NULL)
+        exit(1);
+    if(strcasecmp(argv[1], "read") == 0){
+        int len = sizeof(buffer);
+        if((err = usbhidGetReport(dev, 0, buffer, &len)) != 0){
+            fprintf(stderr, "error reading data: %s\n", usbErrorMessage(err));
+        }else{
+            hexdump(buffer + 1, sizeof(buffer) - 1);
+        }
+    }else if(strcasecmp(argv[1], "write") == 0){
+        int i, pos;
+        memset(buffer, 0, sizeof(buffer));
+        for(pos = 1, i = 2; i < argc && pos < sizeof(buffer); i++){
+            pos += hexread(buffer + pos, argv[i], sizeof(buffer) - pos);
+        }
+        if((err = usbhidSetReport(dev, buffer, sizeof(buffer))) != 0)   /* add a dummy report ID */
+            fprintf(stderr, "error writing data: %s\n", usbErrorMessage(err));
+    }else if(strcasecmp(argv[1], "sleep") == 0) {
+        memset(buffer, 1, sizeof(buffer));
+        if((err = usbhidSetReport(dev, buffer, sizeof(buffer))) != 0)   /* add a dummy report ID */
+            fprintf(stderr, "error writing data: %s\n", usbErrorMessage(err));
+        
+        sleep((int)strtol(argv[2], NULL, 0));
+        memset(buffer, 0, sizeof(buffer));
+        if((err = usbhidSetReport(dev, buffer, sizeof(buffer))) != 0)   /* add a dummy report ID */
+            fprintf(stderr, "error writing data: %s\n", usbErrorMessage(err));
+    } else {
+        usage(argv[0]);
+        exit(1);
+    }
+    usbhidCloseDevice(dev);
+    return 0;
+}
+
+/* ------------------------------------------------------------------------- */
